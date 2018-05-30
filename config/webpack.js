f991b3f7f21707e665c4448da42a15d2f8c1645e
sevenns <a.sevenns@gmail.com>
2018-05-31 00:11:54 +0300

    build 2 versions: with global var and for importing/requiring.

--- config/webpack.js
+++ config/webpack.js
@@ -1,16 +1,12 @@
+// var createVariants = require('parallel-webpack').createVariants;
 var webpack = require('webpack');
 var paths = require('./paths');
 var UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 var FriendlyErrors = require('friendly-errors-webpack-plugin');
 
-module.exports = {
+var config = {
   mode: 'production',
   entry: paths.src,
-  output: {
-    path: paths.dist,
-    filename: 'weery.min.js',
-    libraryTarget: 'commonjs2'
-  },
   resolve: {
     extensions: ['.js']
   },
@@ -43,3 +39,69 @@ module.exports = {
     new webpack.NoEmitOnErrorsPlugin()
   ]
 };
+
+var global = Object.assign({}, config, {
+  output: {
+    path: paths.dist,
+    filename: 'weery.min.global.js',
+    library: '$',
+    libraryTarget: 'global'
+  }
+});
+
+var commonjs = Object.assign({}, config, {
+  output: {
+    path: paths.dist,
+    filename: 'weery.min.module.js',
+    library: 'Weery',
+    libraryTarget: 'commonjs2'
+  }
+});
+
+module.exports = [global, commonjs];
+
+// module.exports = createVariants({
+//   target: ['global', 'commonjs2']
+// }, function (options) {
+//   return {
+//     mode: 'production',
+//     entry: paths.src,
+//     output: {
+//       path: paths.dist,
+//       filename: 'weery.min.' + options.target + '.js',
+//       library: '$',
+//       libraryTarget: options.target
+//     },
+//     resolve: {
+//       extensions: ['.js']
+//     },
+//     module: {
+//       rules: [
+//         {
+//           test: /\.js$/,
+//           exclude: [/node_modules/, paths.dist],
+//           use: ['eslint-loader']
+//         }
+//       ]
+//     },
+//     optimization: {
+//       minimizer: [
+//         new UglifyJsPlugin({
+//           cache: true,
+//           parallel: true,
+//           uglifyOptions: { ecma: 5 }
+//         })
+//       ]
+//     },
+//     plugins: [
+//       new webpack.DefinePlugin({
+//         NODE_ENV: 'production',
+//         __DEV__: 'production'
+//       }),
+//       new FriendlyErrors({
+//         clearConsole: false
+//       }),
+//       new webpack.NoEmitOnErrorsPlugin()
+//     ]
+//   };
+// });
